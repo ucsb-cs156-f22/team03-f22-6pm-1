f8955c40b7cbf4eb294004241f4e0ad380ac18ae
Ethan Mader <thoma@MacBook-Pro-6.local>
2022-11-10 18:56:03 -0800

    em - fixed index page tests

--- frontend/src/tests/pages/HelpRequest/HelpRequestIndexPage.test.js
+++ frontend/src/tests/pages/HelpRequest/HelpRequestIndexPage.test.js
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
 import HelpRequestIndexPage from "main/pages/HelpRequest/HelpRequestIndexPage";
