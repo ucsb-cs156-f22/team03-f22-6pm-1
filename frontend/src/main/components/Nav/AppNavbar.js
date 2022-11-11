fb2eb2f0bb075f2b2c9c6b959e91e93473d7160e
Khang Chung <khangchung@ResNet-58-191.resnet.ucsb.edu>
2022-11-10 17:01:35 -0800

    Added navbar and tests

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -55,6 +55,17 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                   </NavDropdown>
                 )
               }
+
+              {
+                hasRole(currentUser, "ROLE_USER") && (
+                  <NavDropdown title="Articles" id="appnavbar-article-dropdown" data-testid="appnavbar-article-dropdown" >
+                    <NavDropdown.Item as={Link} to="/article/list" data-testid="appnavbar-article-list">List</NavDropdown.Item>
+                    <NavDropdown.Item as={Link} to="/article/create" data-testid="appnavbar-article-create">Create Article</NavDropdown.Item>
+                  </NavDropdown>
+                )
+              }
+
+
                {
                 hasRole(currentUser, "ROLE_USER") && (
                   <NavDropdown title="Dining Commons" id="appnavbar-dining-commons-dropdown" data-testid="appnavbar-dining-commons-dropdown" >
