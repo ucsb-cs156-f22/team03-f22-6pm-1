d7be07f6b4058845d4e213236619ead75e12eb06
dantle1 <76974730+dantle1@users.noreply.github.com>
2022-11-13 22:55:54 -0800

    dl - added fixtures for menu items

---
+++ frontend/src/fixtures/menuItemFixtures.js
@@ -0,0 +1,26 @@
+const menuItemFixtures = {
+    oneMenuItem : {
+        "diningCommonsCode": "carrillo",
+        "name" : "salad",
+        "station" : "greens",
+    },
+    threeMenuItems : [
+        {
+            "diningCommonsCode": "carrillo",
+            "name" : "salad",
+            "station" : "greens",
+        },
+        {
+            "diningCommonsCode": "ortega",
+            "name" : "pizza",
+            "station" : "entrees",
+        },
+        {
+            "diningCommonsCode": "portola",
+            "name" : "burger",
+            "station" : "entree specials",
+        }
+    ]
+}
+
+export { menuItemFixtures };
