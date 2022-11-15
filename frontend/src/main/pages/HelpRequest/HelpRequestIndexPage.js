91d144da225162baa944a89d835d06e170a0d166
Ethan Mader <ethan.mader@gmail.com>
2022-11-14 17:43:41 -0800

    Update HelpRequestIndexPage.js

--- frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
+++ frontend/src/main/pages/HelpRequest/HelpRequestIndexPage.js
@@ -1,27 +1,13 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-import React from 'react'
-import { useBackend } from 'main/utils/useBackend'; // use prefix indicates a React Hook
-import HelpRequestTable from 'main/components/HelpRequest/HelpRequestTable';
-import { useCurrentUser } from 'main/utils/currentUser' // use prefix indicates a React Hook
 
 export default function HelpRequestIndexPage() {
-    
-  const currentUser = useCurrentUser();
-
-  const { data: helpRequests, error: _error, status: _status } =
-    useBackend(
-      // Stryker disable next-line all : don't test internal caching of React Query
-      ["/api/help-request/all"],
-            // Stryker disable next-line StringLiteral,ObjectLiteral : since "GET" is default, "" is an equivalent mutation
-            { method: "GET", url: "/api/help-request/all" },
-      []
-    );
-
   return (
     <BasicLayout>
       <div className="pt-2">
         <h1>Help Requests</h1>
-        <HelpRequestTable helpRequests={helpRequests} currentUser={currentUser} />
+        <p>
+           This is where the index page will go
+        </p>
       </div>
     </BasicLayout>
   )
