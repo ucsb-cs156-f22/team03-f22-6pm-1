97569f3bbb618439a93b65c8842cc117854ef3d3
Khang Chung <khangchung@ResNet-57-34.resnet.ucsb.edu>
2022-11-15 16:11:59 -0800

    KC - finished table, storybook/fixtures, added tests for table and index page

---
+++ frontend/src/fixtures/articleFixtures.js
@@ -0,0 +1,39 @@
+const articleFixtures = {
+    oneArticle: {
+        "id": 1,
+        "title": "title one article",
+        "url": "url one",
+        "explanation": "explanation one article",
+        "email": "khangvchung@ucsb.edu",
+        "dateAdded": "2022-11-15T14:50:44"
+      },
+    threeArticle: [
+        {
+            "id": 1,
+            "title": "title one article",
+            "url": "url one",
+            "explanation": "explanation one article",
+            "email": "khangvchung@ucsb.edu",
+            "dateAdded": "2022-11-15T14:50:44"
+          },
+          {
+            "id": 2,
+            "title": "title two article",
+            "url": "url two",
+            "explanation": "explanation two article",
+            "email": "khangvchung@ucsb.edu",
+            "dateAdded": "2022-11-15T14:50:44"
+          },
+          {
+            "id": 3,
+            "title": "title three article",
+            "url": "url three",
+            "explanation": "explanation three article",
+            "email": "khangvchung@ucsb.edu",
+            "dateAdded": "2022-11-15T14:50:44"
+          }
+    ]
+};
+
+
+export { articleFixtures };
